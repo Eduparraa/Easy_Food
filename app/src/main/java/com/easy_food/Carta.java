cea8882ff62d1d4101b399b0f83f477b14c62847
Benjamin <108248610+Eduparraa@users.noreply.github.com>
2022-09-27 13:52:24 -0300

    Initial commit

--- app/src/main/java/com/easy_food/Carta.java
+++ app/src/main/java/com/easy_food/Carta.java
@@ -6,17 +6,6 @@ import android.os.Bundle;
 
 public class Carta extends AppCompatActivity {
 
-    final FlareBar bottomBar = findViewById(R.id.bottomBar);
-        bottomBar.setBarBackgroundColor(Color.parseColor("#FFFFFF"));
-        ArrayList<Flaretab> tabs = new ArrayList<>();
-        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.inboxb),"Inbox","#FFECB3"));
-        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.searchb),"Search","#80DEEA"));
-        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.phoneb),"Call Log","#B39DDB"));
-        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.avatarb),"Profile","#EF9A9A"));
-        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.settingsb),"Settings","#B2DFDB"));
-
-        bottomBar.setTabList(tabs);
-        bottomBar.attachTabs(MainActivity.this);
 
 
 
@@ -28,4 +17,21 @@ public class Carta extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carta);
     }
+}
+
+public class FlareTab extends AppCompatActivity{
+
+    final FlareBar bottomBar = findViewById(R.id.bottomBar);
+        bottomBar.setBarBackgroundColor(Color.parseColor("#FFFFFF"));
+    ArrayList<Flaretab> tabs = new ArrayList<>();
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.inboxb),"Inbox","#FFECB3"));
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.searchb),"Search","#80DEEA"));
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.phoneb),"Call Log","#B39DDB"));
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.avatarb),"Profile","#EF9A9A"));
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.settingsb),"Settings","#B2DFDB"));
+
+        bottomBar.setTabList(tabs);
+        bottomBar.attachTabs(MainActivity.this);
+
+
 }
